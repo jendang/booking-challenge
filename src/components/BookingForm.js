1f3973ed7dd5f457585447a374f464255c0be368
jennydang <jenny.dang117@gmail.com>
2019-06-12 09:44:25 +0200

    creating BookingForm components

---
+++ src/components/BookingForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+class BookingForm extends React.Component {
+    render(){
+        return (
+            <div>Here is booking form</div>
+        )
+    }
+}
+
+export default BookingForm
